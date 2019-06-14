aaa0076a84aebaa2def96945655d02c64d094f54
yangjian <yangjian102621@gmail.com>
2019-06-14 15:07:54 +0800

    delete mybatis-spring-boot-autoconfigure test files

--- mybatis-spring-boot/mybatis-spring-boot-starter/src/test/java/org/rockyang/mybatis/boot/demo/test/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package org.rockyang.mybatis.boot.demo.test.mapper;
-
-
-import org.rockyang.mybatis.boot.demo.test.model.User;
-import org.rockyang.mybatis.plus.support.BaseMapper;
-
-public interface UserMapper extends BaseMapper<User> {
-
-}
